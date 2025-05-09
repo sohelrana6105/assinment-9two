d1058c7ab30146c84fc19a7e6aedb8336b9bc6e7
sohelrana6105 <sr209195@gmail.com>
2025-05-09 22:44:11 +0600

    fix

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,8 +7,8 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
